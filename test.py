9d860a66783d75f601f6437410327eab5d8d81e7
abhaykumart12 <114352053+abhaykumart12@users.noreply.github.com>
2023-04-24 12:31:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ import os
 import scipy.io
 import yaml
 import math
-from model import ft_net, ft_net_dense, ft_net_hr, ft_net_swin, ft_net_swinv2, ft_net_efficient, ft_net_NAS, ft_net_convnext, PCB, PCB_test
+from model import ft_net
 from utils import fuse_all_conv_bn
 
 ######################################################################
